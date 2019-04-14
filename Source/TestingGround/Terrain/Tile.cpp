2f97aa3b35a6cd3b9a19d3de0f04a30c3c2d4b09
LegendarySpace <40800940+LegendarySpace@users.noreply.github.com>
2019-04-14 01:12:46 -0500

    Added random rotation, scale and other features to Spawn struct

--- Source/TestingGround/Terrain/Tile.cpp
+++ Source/TestingGround/Terrain/Tile.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Tile.h"
+#include "DrawDebugHelpers.h"
 
 // Sets default values
 ATile::ATile()
@@ -8,6 +9,15 @@ ATile::ATile()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+}
+
+void ATile::BeginDestroy()
+{
+	Super::BeginDestroy();
+	for (size_t i = 0; i < Props.Num(); i++)
+	{
+		Props[i]->Destroy();
+	}
 }
 
 // Called when the game starts or when spawned
@@ -21,27 +31,100 @@ void ATile::BeginPlay()
 void ATile::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
-void ATile::PlaceActors(TArray<TSubclassOf<AActor>> ActorsToSpawn, int32 MinSpawn, int32 MaxSpawn)
+void ATile::PlaceActors(TArray<FSpawner> ActorsToSpawn)
+{
+	for (size_t i = 0; i < ActorsToSpawn.Num(); i++)
+	{
+		if (ActorsToSpawn[i].isEmpty()) continue;
+		FSpawner SpawnInfo = ActorsToSpawn[i];
+		TSubclassOf<AActor> ToSpawn = SpawnInfo.SpawnClass;
+		int32 NumberToSpawn = FMath::RandRange(SpawnInfo.SpawnMin, SpawnInfo.SpawnMax);
+
+		for (size_t j = 0; j < NumberToSpawn; j++)
+		{
+			FVector SpawnPoint;
+			float Scale = SpawnInfo.RandomScale ? FMath::FRandRange(SpawnInfo.ScaleMin, SpawnInfo.ScaleMax) : 1.f;
+			// FVector Bounds = SpawnInfo.UniformScale ? {Scale*SpawnInfo.BoundSize.X, Scale*SpawnInfo.BoundSize.Y, Scale*SpawnInfo.BoundSize.Z} : {rand*SpawnInfo.BoundSize.X, rand*SpawnInfo.BoundSize.Y, rand*SpawnInfo.BoundSize.Z}
+			float Radius = FindObjectRadius(ToSpawn, SpawnInfo.BoundSize);
+			if (FindEmptyLocation(SpawnPoint, Radius*Scale))
+			{
+				float Rotation = FMath::FRandRange(-180, 180);
+				Props.Push(PlaceActor(ToSpawn, SpawnPoint, Rotation, Scale));
+			}
+		}
+	}
+}
+
+bool ATile::SpawnAreaClear(FVector Location, float Radius)
+{
+	FVector GlobalLocation = ActorToWorld().TransformPosition(Location);
+	FHitResult HitResult;
+	bool HasHit = GetWorld()->SweepSingleByChannel(
+		HitResult,
+		GlobalLocation,
+		GlobalLocation,
+		FQuat::Identity,
+		ECollisionChannel::ECC_GameTraceChannel2,
+		FCollisionShape::MakeSphere(Radius)
+	);
+
+	FColor ResultColor = HasHit?FColor::Red: FColor::Emerald;
+
+	DrawDebugSphere(GetWorld(), GlobalLocation, Radius, 10, ResultColor, true, 20);
+
+	return !HasHit;
+}
+
+bool ATile::FindEmptyLocation(FVector& OutLocation, float Radius)
 {
 	FVector min = FVector(0, -2000, 0);
 	FVector max = FVector(4000, 2000, 0);
 	FBox bounds = FBox(min, max);
-
-	for (size_t i = 0; i < ActorsToSpawn.Num(); i++)
+	const int MAX_ATTEMPTS = 40;
+	for (size_t i = 0; i < MAX_ATTEMPTS; i++)
 	{
-		int32 NumberToSpawn = FMath::RandRange(MinSpawn, MaxSpawn);
-		TSubclassOf<AActor> ToSpawn = ActorsToSpawn[i];
-		for (size_t i = 0; i < NumberToSpawn; i++)
+		FVector SpawnPoint = FMath::RandPointInBox(bounds);
+		if (SpawnAreaClear(SpawnPoint, Radius))
 		{
-			FVector SpawnPoint = FMath::RandPointInBox(bounds);
-			UE_LOG(LogTemp, Warning, TEXT("SpawnPoint: %s"), *SpawnPoint.ToCompactString());
-			AActor* Spawned = GetWorld()->SpawnActor<AActor>(ToSpawn, SpawnPoint, FRotator::ZeroRotator);
-			Spawned->AttachToActor(this, FAttachmentTransformRules(EAttachmentRule::KeepRelative, false));
-
+			OutLocation = SpawnPoint;
+			return true;
 		}
 	}
+	return false;
+}
+
+AActor* ATile::PlaceActor(TSubclassOf<AActor> ToSpawn, FVector Location, float Rotation, float Scale)
+{
+	FRotator Rot = FRotator(0.f, Rotation, 0.f);
+	AActor* Spawned = GetWorld()->SpawnActor<AActor>(ToSpawn);
+	Spawned->SetActorRelativeLocation(Location);
+	Spawned->AttachToActor(this, FAttachmentTransformRules(EAttachmentRule::KeepRelative, false));
+	Spawned->SetActorScale3D(FVector(Scale));
+	Spawned->SetActorRotation(Rot);
+	return Spawned;
+}
+
+float ATile::FindObjectRadius(TSubclassOf<AActor> ClassToCheck, FVector DeltaBounds)
+{
+	float X2, Y2, Z2, Radius;
+	if (DeltaBounds == FVector::ZeroVector)
+	{
+		/** Determine Delta from object **/
+		// Spawn Actor
+		AActor* TestObject = GetWorld()->SpawnActor<AActor>(ClassToCheck);
+		// DeltaBounds = GetBoundsExtent() - GetBoundsOrigin()
+		FVector Origin, Extent;
+		TestObject->GetActorBounds(true, Origin, Extent);
+		DeltaBounds = Extent - Origin;
+		TestObject->Destroy();
+	}
+	// Radius = SqRt(DeltaX^2 + DeltaY^2 + DeltaZ^2)
+	X2 = FMath::Square<float>(DeltaBounds.X);
+	Y2 = FMath::Square<float>(DeltaBounds.Y);
+	Z2 = FMath::Square<float>(DeltaBounds.Z);
+	Radius = FMath::Sqrt(X2 + Y2 + Z2);
+	return Radius;
 }
 
--- Source/TestingGround/Terrain/Tile.h
+++ Source/TestingGround/Terrain/Tile.h
@@ -6,10 +6,75 @@
 #include "GameFramework/Actor.h"
 #include "Tile.generated.h"
 
+USTRUCT(BlueprintType)
+struct FSpawner
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Setup)
+	TSubclassOf<AActor> SpawnClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Setup)
+	int SpawnMin;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Setup)
+	int SpawnMax;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Setup)
+	float ScaleMin;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Setup)
+	float ScaleMax;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Setup)
+	bool RandomScale;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Setup)
+	bool UniformScale;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Setup)
+	FVector BoundSize;
+
+	FSpawner()
+	{
+		SpawnClass = NULL;
+		SpawnMin = 0;
+		SpawnMax = 7;
+		ScaleMin = .25;
+		ScaleMax = 4;
+		RandomScale = false;
+		UniformScale = true;
+		BoundSize = FVector::ZeroVector;
+	}
+	
+	bool operator==(const FSpawner& rightValue)
+	{
+		if (rightValue.SpawnClass == NULL) return false;
+		return true;
+	}
+
+	bool operator==(const int& rightValue)
+	{
+		if (rightValue == NULL) return false;
+		return true;
+	}
+
+	bool isEmpty()
+	{
+		if (SpawnClass == NULL) return true;
+		return false;
+	}
+};
+
 UCLASS()
 class TESTINGGROUND_API ATile : public AActor
 {
 	GENERATED_BODY()
+
+private:
+	TArray<AActor*> Props = TArray<AActor*>();
 	
 public:	
 	// Sets default values for this actor's properties
@@ -19,11 +84,26 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	// Called before the actor is destroyed
+	virtual void BeginDestroy() override;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	UFUNCTION(BlueprintCallable, Category = setup)
-	void PlaceActors(TArray<TSubclassOf<AActor>> ActorsToSpawn, int32 MinSpawn, int32 MaxSpawn);
+	void PlaceActors(TArray<FSpawner> ActorsToSpawn);
+
+	UFUNCTION(BlueprintCallable, Category = setup)
+	bool SpawnAreaClear(FVector Location, float Radius);
+
+	UFUNCTION(BlueprintCallable, Category = setup)
+	bool FindEmptyLocation(FVector& OutLocation, float Radius);
+
+	UFUNCTION(BlueprintCallable, Category = setup)
+	AActor* PlaceActor(TSubclassOf<AActor> ToSpawn, FVector Location, float Rotation, float Scale);
+
+	UFUNCTION(BlueprintCallable, Category = setup)
+	float FindObjectRadius(TSubclassOf<AActor> ClassToCheck, FVector DeltaBounds = FVector::ZeroVector);
 
 };
